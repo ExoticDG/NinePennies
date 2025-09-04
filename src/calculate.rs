6c117ef996fc7ff717e4dc39b73aa2b096d55ef0
ExoticDG <110739962+ExoticDG@users.noreply.github.com>
2025-09-03 19:02:45 -0500

    Update calculate.rs

--- src/calculate.rs
+++ src/calculate.rs
@@ -1,13 +1,23 @@
+use std::result;
+
 
 
 pub fn calculate_first_query(total_length_f32: f32, first_character: String, second_character: String, third_character: String, fourth_character: String,) -> String {
 
 
-    let a:f32 = total_length_f32 / 12.0;
+    //TODO: Calculate how many characters of each type there are in the array
+
+    // let a:f32 = total_length_f32 / 12.0;
+
+    // let b:i32 = a as i32;
+
+    // let c:f32 = a - (b as f32);
 
 
+    // let d = b * 12;
 
-    let result = String::from(a.to_string());
+    // let result = format!("{} , {} , {}", a, b, c);
 
+    let result = ("temp").to_string();
     return(result);
 }
