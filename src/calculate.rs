8666a2c918838b75b9ec585ba629bc392a041fd3
ExoticDG <110739962+ExoticDG@users.noreply.github.com>
2025-09-04 12:50:43 -0500

    Switched arrays for vectors. Also, it works now.

--- src/calculate.rs
+++ src/calculate.rs
@@ -26,13 +26,43 @@ pub fn calculate_first_query(total_length_f32: f32, first_character: String, sec
 
     let mut result_vector = base_vector;
 
-    for i in 0..result_vector.len() {
-        // Check if the current index (plus 1 for 1-based counting) is a multiple of the interval
-        if (i + 1) % intone == 2 {
-            // Modify the value at the current index
-            result_vector[i] = 1; // Set the value to 0 for demonstration
+
+    for (index, value) in result_vector.iter_mut().enumerate() {
+        // Check if the current index (plus 1 for 1-based counting) is a multiple of n
+        if (index + 1) % intone == 0 {
+            *value = 1; // Modify the value, for example, set it to 0
         }
     }
+
+    for (index, value) in result_vector.iter_mut().enumerate() {
+        // Check if the current index (plus 1 for 1-based counting) is a multiple of n
+        if (index + 1) % inttwo == 0 {
+            *value = 2; // Modify the value, for example, set it to 0
+        }
+    }
+
+    for (index, value) in result_vector.iter_mut().enumerate() {
+        // Check if the current index (plus 1 for 1-based counting) is a multiple of n
+        if (index + 1) % intthree == 0 {
+            *value = 3; // Modify the value, for example, set it to 0
+        }
+    }
+
+
+    // for //(index, value) in result_vector.iter_mut().enumerate() {
+    //     // Check if the current index (plus 1 for 1-based counting) is a multiple of n
+    //     if (index + 1) % intone == 0
+    //         *value = 1 // Modify the value, for example, set it to 0
+    //     //}
+    
+
+    // for i in 2..result_vector.len() {
+    //     // Check if the current index (plus 1 for 1-based counting) is a multiple of the interval
+    //     if (i + 1) % intone == 2 {
+    //         // Modify the value at the current index
+    //         result_vector[i] = 1; // Set the value to 0 for demonstration
+    //     }
+    // }
 
     let result = (result_vector); //("temp").to_string();
 
